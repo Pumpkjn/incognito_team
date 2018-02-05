d11d3e51651188e57b84301c432524f31d32fac5
nguyenvu9405 <nguyenvu9405@gmail.com>
2018-02-05 03:50:34 +0000

    homepage 5 feb

--- templates/home.php
+++ templates/home.php
@@ -4,14 +4,17 @@
     <title>Bootstrap 101 Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 
+<!-- Top navigation -->
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
     <div class="container">
+        <!-- Logo + toggle button -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapsable-menu">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -19,8 +22,8 @@
             </button>
             <a class="navbar-brand" href="#">Incognito</a>
         </div>
-
-        <div class="collapse navbar-collapse">
+        <!-- Collapseable menu -->
+        <div class="collapse navbar-collapse" id="collapsable-menu">
             <ul class="nav navbar-nav">
                 <li class="active">
                     <a href="#">Home</a>
@@ -61,12 +64,168 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-xs-12 col-md-9 page-header-2">
-            <h2>Latest idea</h2>
-            <h2>Departments</h2>
+        <div class="col-xs-12 col-md-9">
+            <h2 class="page-header-2">Latest idea</h2>
+            <div class="col-xs-12">
+                <!-- the start of an idea -->
+                <div class="panel panel-default">
+                    <div class="panel-heading ">
+                        <h4 class="pull-left">Title</h4>
+                        <div class="cats pull-right ">
+                            <h5>Categories:
+                                <a href="#"><span class="label label-primary">computing</span></a>
+                                <a href="#"><span class="label label-primary">life</span></a>
+                                <a href="#"><span class="label label-primary">love</span></a>
+                            </h5>
+
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="panel-body">
+                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam aspernatur beatae
+                            corporis doloribus eligendi, enim esse ex ipsa magnam modi nihil nostrum, odio tempora
+                            voluptas. Aperiam odit recusandae soluta!
+                        </div>
+                        <div>Alias amet, ducimus earum excepturi harum inventore, nobis obcaecati odit officiis
+                            praesentium quaerat quas quibusdam totam vero vitae! Dolore dolores ducimus fugit laudantium
+                            numquam placeat possimus soluta suscipit voluptatem, voluptates!
+                        </div>
+                        <div>Adipisci animi aut, eos et laborum laudantium maiores maxime molestias nam possimus quo
+                            repellendus saepe, velit? Alias, dolor eum ex fuga illo illum inventore iste libero, minima
+                            nesciunt praesentium tempore.
+                        </div>
+                        <div>At, corporis deleniti dolor eos et expedita explicabo fugiat id in inventore ipsam maiores
+                            minus, modi nisi perferendis perspiciatis, porro praesentium quis quod repellat
+                            reprehenderit sed similique sit unde voluptatem?
+                        </div>
+                        <div>A aliquam amet aperiam asperiores dicta enim eos et fugiat illum impedit in itaque iusto
+                            minima nobis odit officia officiis, qui ratione repudiandae sed soluta tempora unde
+                            voluptatem. Assumenda, eum.
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        <ul class="list-inline pull-left">
+                            <li>
+                                <a>
+                                    <span class="glyphicon glyphicon-thumbs-up"></span>
+                                    <span> Up </span>
+                                </a>
+                            </li>
+                            <li>|</li>
+                            <li>
+                                <a>
+                                    <span class="glyphicon glyphicon-thumbs-down"></span>
+                                    <span> Down </span>
+                                </a>
+                            </li>
+                            <li>|</li>
+                            <li>
+                                <a>
+                                   <span class="glyphicon glyphicon-comment"></span>
+                                    <span>2 comments</span>
+                                </a>
+                        </ul>
+                        <ul class="list-inline pull-right">
+                            <li>
+                                <span class="glyphicon glyphicon-calendar"></span>
+                                2 days, 8 hours ago
+                            </li>
+                            <li>|</li>
+                            <li>
+                                <span class="glyphicon glyphicon-user"></span>
+                                by
+                                <a href="#">
+                                    John
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <!-- The end of an idea -->
+                <!-- The beginning of an idea -->
+                <div class="panel panel-default">
+                    <div class="panel-heading ">
+                        <h4 class="pull-left">Title</h4>
+                        <div class="cats pull-right ">
+                            <h5>Categories:
+                                <a href="#"><span class="label label-primary">computing</span></a>
+                                <a href="#"><span class="label label-primary">life</span></a>
+                                <a href="#"><span class="label label-primary">love</span></a>
+                            </h5>
+
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="panel-body">
+                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam aspernatur beatae
+                            corporis doloribus eligendi, enim esse ex ipsa magnam modi nihil nostrum, odio tempora
+                            voluptas. Aperiam odit recusandae soluta!
+                        </div>
+                        <div>Alias amet, ducimus earum excepturi harum inventore, nobis obcaecati odit officiis
+                            praesentium quaerat quas quibusdam totam vero vitae! Dolore dolores ducimus fugit laudantium
+                            numquam placeat possimus soluta suscipit voluptatem, voluptates!
+                        </div>
+                        <div>Adipisci animi aut, eos et laborum laudantium maiores maxime molestias nam possimus quo
+                            repellendus saepe, velit? Alias, dolor eum ex fuga illo illum inventore iste libero, minima
+                            nesciunt praesentium tempore.
+                        </div>
+                        <div>At, corporis deleniti dolor eos et expedita explicabo fugiat id in inventore ipsam maiores
+                            minus, modi nisi perferendis perspiciatis, porro praesentium quis quod repellat
+                            reprehenderit sed similique sit unde voluptatem?
+                        </div>
+                        <div>A aliquam amet aperiam asperiores dicta enim eos et fugiat illum impedit in itaque iusto
+                            minima nobis odit officia officiis, qui ratione repudiandae sed soluta tempora unde
+                            voluptatem. Assumenda, eum.
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        <ul class="list-inline pull-left">
+                            <li>
+                                <a>
+                                    <span class="glyphicon glyphicon-thumbs-up"></span>
+                                    <span> Up </span>
+                                </a>
+                            </li>
+                            <li>|</li>
+                            <li>
+                                <a>
+                                    <span class="glyphicon glyphicon-thumbs-down"></span>
+                                    <span> Down </span>
+                                </a>
+                            </li>
+                            <li>|</li>
+                            <li>
+                                <a>
+                                    <span class="glyphicon glyphicon-comment"></span>
+                                    <span>2 comments</span>
+                                </a>
+                        </ul>
+                        <ul class="list-inline pull-right">
+                            <li>
+                                <span class="glyphicon glyphicon-calendar"></span>
+                                2 days, 8 hours ago
+                            </li>
+                            <li>|</li>
+                            <li>
+                                <span class="glyphicon glyphicon-user"></span>
+                                by
+                                <a href="#">
+                                    John
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <!-- The end of an idea -->
+            </div>
+            <div class="clearfix visible-xs-block"></div>
+            <h2 class="page-header-2">Departments</h2>
         </div>
 
         <div class="col-xs-12 col-md-3 ">
+            <!-- Search box-->
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">Search</h3>
@@ -76,49 +235,50 @@
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Enter text here">
                         </div>
-                        <input type="submit" class="btn btn-default"></input>
+                        <input type="submit" class="btn btn-default">
                     </form>
                 </div>
             </div>
+            <!-- Categories -->
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">Categories</h3>
                 </div>
-                <div class="panel-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                            <span class="badge">14</span>
-                            Computer upgrade
-                        </li>
-                        <li class="list-group-item">
-                            <span class="badge">90</span>
-                            Distance learning
-                        </li>
-                        <li class="list-group-item">
-                            <span class="badge">50</span>
-                            Parking improvement
-                        </li>
-                    </ul>
-                </div>
+
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <span class="badge">14</span>
+                        <a>Computer upgrade </a>
+                    </li>
+                    <li class="list-group-item">
+                        <span class="badge">90</span>
+                        <a>Distance learning</a>
+                    </li>
+                    <li class="list-group-item">
+                        <span class="badge">50</span>
+                        <a>Parking improvement</a>
+                    </li>
+                </ul>
             </div>
+            <!-- Popular idea -->
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">Popular ideas</h3>
                 </div>
-                <div class="panel-body">
-                    <div class="list-group">
-                        <a href="#" class="list-group-item">
-                            <h4 class="list-group-item-heading">Computer upgrade</h4>
-                            <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt dolorem dolorem</p>
-                        </a>
-                    </div>
-                    <div class="list-group">
-                        <a href="#" class="list-group-item">
-                            <h4 class="list-group-item-heading">Distance learning</h4>
-                            <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt dolorem dolorem</p>
-                        </a>
-                    </div>
+
+                <div class="list-group">
+                    <a href="#" class="list-group-item">
+                        <h4 class="list-group-item-heading">Computer upgrade</h4>
+                        <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt dolorem dolorem</p>
+                    </a>
                 </div>
+                <div class="list-group">
+                    <a href="#" class="list-group-item">
+                        <h4 class="list-group-item-heading">Distance learning</h4>
+                        <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt dolorem dolorem</p>
+                    </a>
+                </div>
+
             </div>
         </div>
     </div>
