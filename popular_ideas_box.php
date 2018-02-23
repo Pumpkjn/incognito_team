234cc30651993181534fb9df9d57ec4298b1f7bd
nguyenvu9405 <nguyenvu9405@gmail.com>
2018-02-23 15:07:58 +0000

    front-end

--- popular_ideas_box.php
+++ popular_ideas_box.php
@@ -5,13 +5,13 @@
     </div>
 
     <div class="list-group">
-        <a href="#" class="list-group-item">
+        <a href="idea.php" class="list-group-item">
             <h4 class="list-group-item-heading">Computer upgrade</h4>
             <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt dolorem dolorem</p>
         </a>
     </div>
     <div class="list-group">
-        <a href="#" class="list-group-item">
+        <a href="idea.php" class="list-group-item">
             <h4 class="list-group-item-heading">Distance learning</h4>
             <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt dolorem dolorem</p>
         </a>
